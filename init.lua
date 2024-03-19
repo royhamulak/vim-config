9c8267540c83b84f215fe821e89d9a4101952629
Roy <8260897+royhamulak@users.noreply.github.com>
2024-03-19 11:18:50 +0200

    plugins started

--- init.lua
+++ init.lua
@@ -1,6 +1,6 @@
--- vim.cmd('source $VIM_CONFIG/.vimrc')
+vim.cmd('source $VIM_CONFIG/.vimrc')
 
 -- Add VIM_CONFIG to runtimepath
-vim.o.runtimepath = vim.o.runtimepath .. ',' .. os.getenv('VIM_CONFIG')
-
-require('core')
+-- vim.o.runtimepath = vim.o.runtimepath .. ',' .. os.getenv('VIM_CONFIG')
+--
+-- require('core')
---
+++ lua/core/plugins/onedark.lua
@@ -0,0 +1,14 @@
+return {
+  "navarasu/onedark.nvim",
+  config = function ()
+    require('onedark').setup({
+        style = 'warmer',
+        transparent = true,
+        highlights = {
+          ["@type"] = {fg = '$red'},
+          ["@variable.parameter"] = {fg= '$yellow'},
+        },
+    })
+    vim.cmd('colorscheme onedark')
+  end
+}
