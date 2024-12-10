f6c9a90064d15281674c06b050ade51de7650dfe
Roy <8260897+royhamulak@users.noreply.github.com>
2024-12-10 17:46:52 +0200

    updated arrow

--- lua/lentent/plugins/arrow/init.lua
+++ lua/lentent/plugins/arrow/init.lua
@@ -1,10 +1,13 @@
 return createButPlugConfig({
-	"otavioschwanck/arrow.nvim",
-	config = function()
-		require("arrow").setup({
-			always_show_path = true,
-			show_icons = true,
-			leader_key = "\\",
-		})
-	end,
+  "otavioschwanck/arrow.nvim",
+  dependencies = {
+    { "nvim-tree/nvim-web-devicons" },
+    -- or if using `mini.icons`
+    -- { "echasnovski/mini.icons" },
+  },
+  opts = {
+    always_show_path = true,
+    show_icons = true,
+    leader_key = "\\",
+  },
 })
