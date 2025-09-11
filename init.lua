b0289ba468d692c0deb1bfd54fe828c687f9eac9
Roy <royhamulak@gmail.com>
2025-09-11 19:59:51 +0300

    better config path

--- init.lua
+++ init.lua
@@ -1,5 +1,6 @@
-vim.o.runtimepath = vim.o.runtimepath .. ',' .. os.getenv('VIM_CONFIG')
-vim.cmd("source $VIM_CONFIG/.vimrc")
+local VIM_CONFIG = os.getenv("VIM_CONFIG") or (os.getenv("SUDO_HOME") .. "/.config/nvim")
+vim.o.runtimepath = vim.o.runtimepath .. "," .. VIM_CONFIG
+vim.cmd("source " .. VIM_CONFIG .. "/.vimrc")
 --
 -- Add VIM_CONFIG to runtimepath
 
